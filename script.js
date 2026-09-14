6c6893b031127ded7319654c7b1db0cd8d45f310
oliveiralimaester-hub <oliveira.lima.ester@escola.pr.gov.br>
2026-09-14 10:40:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const botoes= document.querySector("button");
+  
+ botoes.forEach(funcion(botao); {
+   let curtiu = false;
+  botao.addEventlistener("click, botaoClicado);
+  function botaoClicado() {
+  console.log("fui clicado");
+  let texto = botao.querySelector("span");
+  if (curtiu === false) {
 
+  }
+  texto.textContent++;
+}
+  });
